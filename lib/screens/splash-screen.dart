a3b9a53c09cdb7948c59d93d2834cf7ff8a458b8
thushaltk <thushal1999@gmail.com>
2022-08-11 01:30:42 +0530

    UPDATED: getting started screen

--- lib/screens/splash-screen.dart
+++ lib/screens/splash-screen.dart
@@ -13,7 +13,7 @@ class SplashScreen extends StatelessWidget {
           width: double.infinity,
           height: double.infinity,
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
@@ -24,15 +24,87 @@ class SplashScreen extends StatelessWidget {
                       'assets/images/pattern.png',
                       fit: BoxFit.contain),
                 ),
-                const SizedBox(height: 20),
                 Container(
                   alignment: Alignment.center,
                   width: double.infinity,
                   child: Image.asset(
-                      width: 230,
+                      width: 280,
                       'assets/images/logo.png',
                       fit: BoxFit.contain),
-                )
+                ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    width: 150,
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        )),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color(0xFF393737)),
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          const EdgeInsets.all(18.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        // Navigator.of(context)
+                        //     .pushNamed(LoginSelectionScreen.routeName);
+                      },
+                      child: const Text(
+                        'GET STARTED',
+                        style: TextStyle(
+                          fontSize: 13,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      width: double.infinity,
+                      child: Image.asset(
+                          width: 330,
+                          'assets/images/pattern2.png',
+                          fit: BoxFit.contain),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 150,
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'Who we are?',
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12),
+                          ),
+                        ),
+                        Container(
+                          height: 150,
+                          alignment: Alignment.bottomCenter,
+                          child: const Text(
+                            'What we do?',
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ]),
         ));
   }
