40d6d8aa13f826a24ef4cd8afaa0fcc4c8558ccd
thushaltk <thushal1999@gmail.com>
2022-08-22 01:32:58 +0530

    UPDATED: login screen

--- lib/screens/login-screen.dart
+++ lib/screens/login-screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -16,43 +17,122 @@ class LoginScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  child: Image.asset(
-                      width: 280,
-                      'assets/images/logo.png',
-                      fit: BoxFit.contain),
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    width: 150,
-                    height: 40,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        )),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color(0xFF393737)),
-                        padding: MaterialStateProperty.all<EdgeInsets>(
-                          const EdgeInsets.all(18.0),
-                        ),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        width: double.infinity,
+                        child: Image.asset(
+                            width: 280,
+                            'assets/images/logo.png',
+                            fit: BoxFit.contain),
                       ),
-                      onPressed: () {
-                        // Navigator.of(context)
-                        //     .pushNamed(LoginSelectionScreen.routeName);
-                      },
-                      child: const Text(
-                        'GET STARTED',
-                        style: TextStyle(
-                          fontSize: 13,
-                        ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: SizedBox(
+                              width: 250,
+                              child: ElevatedButton.icon(
+                                icon: const Icon(Icons.facebook),
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          const Color(0xFF393737)),
+                                  padding:
+                                      MaterialStateProperty.all<EdgeInsets>(
+                                    const EdgeInsets.all(18.0),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  // Navigator.of(context)
+                                  //     .pushNamed(LoginSelectionScreen.routeName);
+                                },
+                                label: const Text(
+                                  'LINK FACEBOOK ACCOUNT',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: ColoredBox(
+                              color: Colors.white,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: const [
+                                  Flexible(
+                                    child: Divider(color: Colors.grey),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    'or',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Flexible(
+                                    child: Divider(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.center,
+                            child: SizedBox(
+                              width: 250,
+                              child: ElevatedButton.icon(
+                                icon: const Icon(FontAwesomeIcons.twitter),
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          const Color(0xFF393737)),
+                                  padding:
+                                      MaterialStateProperty.all<EdgeInsets>(
+                                    const EdgeInsets.all(18.0),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  // Navigator.of(context)
+                                  //     .pushNamed(LoginSelectionScreen.routeName);
+                                },
+                                label: const Text(
+                                  'LINK TWITTER ACCOUNT',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                    ],
                   ),
                 ),
                 Stack(
