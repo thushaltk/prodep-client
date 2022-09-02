2a564e566cbe48ad35470575dff0d92c928a9d1e
thushaltk <thushal1999@gmail.com>
2022-09-03 03:22:34 +0530

    updated

---
+++ lib/widgets/singleday-widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SingleDayWidget extends StatefulWidget {
+  final String day;
+
+  const SingleDayWidget({Key? key, required this.day}) : super(key: key);
+
+  @override
+  State<SingleDayWidget> createState() => _SingleDayWidgetState();
+}
+
+class _SingleDayWidgetState extends State<SingleDayWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 50,
+        height: 70,
+        child: Text(widget.day),
+      ),
+    );
+  }
+}
